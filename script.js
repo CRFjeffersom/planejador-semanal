e216fc2cf71c47ffb601243b65123003a89bed63
Jeffersom <Jeffersombenvindo@gmail.com>
2022-02-20 20:14:02 -0300

    versao 1.0

---
+++ script.js
@@ -0,0 +1,67 @@
+function AdcionarDomingo(){
+   var tarefa = window.prompt('Adicione sua Tarefa')
+   const para = document.createElement('p')
+   para.innerHTML = (tarefa)
+   document.getElementById("TarefaDomingo").appendChild(para)
+   localStorage.setItem(tarefa);
+   
+}
+
+function AdcionarSegunda(){
+   var tarefa = window.prompt('Adicione sua Tarefa')
+   const para = document.createElement('p')
+   para.innerHTML = (tarefa)
+   document.getElementById("TarefaSegunda").appendChild(para)
+}
+
+function AdcionarTerça(){
+   var tarefa = window.prompt('Adicione sua Tarefa')
+   const para = document.createElement('p')
+   para.innerHTML = (tarefa)
+   document.getElementById("TarefaTerça").appendChild(para)
+}
+
+function AdcionarQuarta(){
+      var tarefa = window.prompt('Adicione sua Tarefa')
+      const para = document.createElement('p')
+      para.innerHTML = (tarefa)
+      document.getElementById("TarefaQuarta").appendChild(para)
+}
+
+function AdcionarQuinta(){
+   var tarefa = window.prompt('Adicione sua Tarefa')
+   const para = document.createElement('p')
+   para.innerHTML = (tarefa)
+   document.getElementById("TarefaQuinta").appendChild(para)
+}
+
+function AdcionarSexta(){
+   var tarefa = window.prompt('Adicione sua Tarefa')
+   const para = document.createElement('p')
+   para.innerHTML = (tarefa)
+   document.getElementById("TarefaSexta").appendChild(para)
+}
+
+function AdcionarSabado(){
+   var tarefa = window.prompt('Adicione sua Tarefa')
+   const para = document.createElement('p')
+   para.innerHTML = (tarefa)
+   document.getElementById("TarefaSabado").appendChild(para)
+}
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
